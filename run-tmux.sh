b4dd363a495173c5d7a147ee58da5e344bb6eb75
takleung <takleung@gmail.com>
2022-12-03 22:19:03 +0800

    name 4th change

--- run-tmux.sh
+++ run-tmux.sh
@@ -12,14 +12,14 @@ SERVER=$1
 
 if [ -z $TMUX ] ; then
   echo "Start new tmux session"
-  tmux new-session -d -s demo-$PID "watch curl -s -k https://homer-simpson.${SERVER}/demo"
+  tmux new-session -d -s demo-$PID "watch curl -s -k https://johnny-rhhk.${SERVER}/demo"
 fi
-tmux split-window -d -t 0 -v  "watch curl -s -k https://marge-simpson.${SERVER}/demo"
-tmux split-window -d -t 0 -h  "watch curl -s -k https://selma-bouvier.${SERVER}/demo"
-tmux split-window -d -t 2 -h  "watch curl -s -k https://patty-bouvier.${SERVER}/demo"
+tmux split-window -d -t 0 -v  "watch curl -s -k https://peter-rhhk.${SERVER}/demo"
+tmux split-window -d -t 0 -h  "watch curl -s -k https://bill-hkex.${SERVER}/demo"
+tmux split-window -d -t 2 -h  "watch curl -s -k https://marco-hkex.${SERVER}/demo"
 if [ -z $TMUX ] ; then
   echo "Attach to session"
   tmux attach-session -t demo-$PID
 else
-  watch curl -s -k http://homer-simpson.${SERVER}/demo
+  watch curl -s -k http://johnny-rhhk.${SERVER}/demo
 fi
